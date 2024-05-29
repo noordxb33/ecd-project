08fb6ccd636a31544ef02257cf195a966d88c1c7
usrabrdxb <166487428+usrabrdxb@users.noreply.github.com>
2024-05-29 15:20:09 +0430

    new changes

---
+++ app/Http/Controllers/MotherQualificationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\MotherQualification;
+
+class MotherQualificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            "name"=> "string|required",
+        ]);
+        $motherQualification = MotherQualification::create($request->all());
+        return response()->json($motherQualification,200);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
